f51f7951a7091497d746f7513e1f61e2001042cd
Adi <23616271+maladi17@users.noreply.github.com>
2019-12-21 23:28:31 +0200

    edited name

--- privEsc.bat
+++ privEsc.bat
@@ -6,17 +6,11 @@ systeminfo > systeminfo.txt 2> nul
 find "KB" systeminfo.txt > hotfix.txt 2> nul
 cls
 :MENU
-echo " _       ___       ____       _       ______
-echo "| |     / (_)___  / __ \_____(_)   __/ ____/_________
-echo "| | /| / / / __ \/ /_/ / ___/ / | / / __/ / ___/ ___/
-echo "| |/ |/ / / / / / ____/ /  / /| |/ / /___(__  ) /__
-echo "|__/|__/_/_/ /_/_/   /_/  /_/ |___/_____/____/\___/
-echo.
 echo Windows Enumeration and Privilege Escalation Script
 echo www.joshruppe.com ^| Twitter: @josh_ruppe ^| https://github.com/joshruppe/winprivesc/blob/master/winprivesc.bat
 echo https://xapax.gitbooks.io/security/content/privilege_escalation_windows.html ^| xapax.gitbooks
 echo https://addaxsoft.com/wpecs/ ^| the Open Source Windows Previlege Escalation Cheat Sheet
-echo https://hackmag.com/security/elevating-privileges-to-administrative-and-further/ ^| TOP–10 ways to boost your privileges in Windows systems
+echo https://hackmag.com/security/elevating-privileges-to-administrative-and-further/ ^| TOPâ€“10 ways to boost your privileges in Windows systems
 echo https://decoder.cloud/2017/02/21/the-system-challenge/ ^| decoders blog
 echo https://decoder.cloud/2017/02/21/the-system-challenge/
 echo https://www.absolomb.com/2018-01-26-Windows-Privilege-Escalation-Guide/ ^| absolomb
@@ -34,22 +28,10 @@ rem nc -l -p 1234 > report.txt on our computer , nc -l -p 5555 > jaws.txt on our
 
 
 echo 1 - All to Report
-echo 2 - Operating System
-echo 3 - Storage
-echo 4 - Networking
-echo 5 - Processess
-echo 6 - User Info
-echo 7 - Exit
-echo.
+
 SET /P C=Select^>
 echo.
 IF %C%==1 GOTO ALL
-IF %C%==2 GOTO OS
-IF %C%==3 GOTO STORAGE
-IF %C%==4 GOTO NETWORK
-IF %C%==5 GOTO PROCESSES
-IF %C%==6 GOTO USERS
-IF %C%==7 GOTO EXIT
 
 :ALL
 echo WinPrivEsc > report.txt
@@ -211,11 +193,11 @@ echo.>> report.txt
 echo.>> report.txt
 echo The most pleasant thing is to see SERVICE_ALL_ACCESS permission for the authenticate and power users. However, there could be another good variants: >> report.txt
 
-echo SERVICE_CHANGE_CONFIG — can change service run file; >> report.txt
-echo WRITE_DAC — allows to change permissions which might lead to SERVICE_CHANGE_CONFIG;>> report.txt
-echo WRITE_OWNER — you can become an owner by means of it;>> report.txt
-echo GENERIC_WRITE — inherits SERVICE_CHANGE_CONFIG permissions;>> report.txt
-echo GENERIC_ALL — inherits SERVICE_CHANGE_CONFIG permissions.>> report.txt
+echo SERVICE_CHANGE_CONFIG â€” can change service run file; >> report.txt
+echo WRITE_DAC â€” allows to change permissions which might lead to SERVICE_CHANGE_CONFIG;>> report.txt
+echo WRITE_OWNER â€” you can become an owner by means of it;>> report.txt
+echo GENERIC_WRITE â€” inherits SERVICE_CHANGE_CONFIG permissions;>> report.txt
+echo GENERIC_ALL â€” inherits SERVICE_CHANGE_CONFIG permissions.>> report.txt
 echo.>> report.txt
 echo.>> report.txt
 echo.>> report.txt
@@ -679,143 +661,7 @@ del Servicenames.txt
 del hotfix.txt
 EXIT /B
 
-:OS
-echo __________________________
-echo.
-echo      OPERATING SYSTEM
-echo __________________________
-echo.
-echo [++OS Name]
-echo.
-for /F "tokens=3-7" %%a IN ('find /i "OS Name:" systeminfo.txt') do set Name=%%a %%b %%c %%d %%e
-echo %Name%
-echo.
-echo [++OS Version]
-echo.
-for /F "tokens=3-6" %%a IN ('findstr /B /C:"OS Version:" systeminfo.txt') do set Version=%%a %%b %%c %%d
-echo %Version%
-echo.
-echo [++System Architecture]
-echo.
-for /F "tokens=3-4"  %%a IN ('findstr /B /C:"System Type:" systeminfo.txt') do set Type=%%a %%b
-echo %Type%
-echo.
-echo [++System Boot Time]
-echo.
-for /F "tokens=4-6" %%a IN ('findstr /B /C:"System Boot Time:" systeminfo.txt') do set UpTime=%%a %%b %%c
-echo %UpTime%
-echo.
-echo [++Page File Location(s)]
-echo.
-for /F "tokens=4" %%a IN ('findstr /B /C:"Page File Location(s):" systeminfo.txt') do set Page=%%a
-echo %Page%
-echo.
-echo [++Hotfix(s) Installed]
-echo.
-setlocal enabledelayedexpansion
-for /F "tokens=2" %%a IN ('findstr /v ".TXT" hotfix.txt') do (
-  set Hot=%%~a
-  echo !Hot!
-)
-echo.
-echo [++Hosts File]
-echo.
-more c:\WINDOWS\System32\drivers\etc\hosts
-echo.
-echo [++Networks File]
-echo.
-more c:\WINDOWS\System32\drivers\etc\networks
-echo.
-echo [++Running Services]
-echo.
-net start
-echo.
-del systeminfo.txt
-del hotfix.txt
-EXIT /B
 
-:STORAGE
-echo _________________
-echo.
-echo      STORAGE
-echo _________________
-echo.
-echo [++Physical Drives]
-net share
-echo.
-echo [++Network Drives]
-echo.
-net use
-del systeminfo.txt
-del hotfix.txt
-EXIT /B
-
-:NETWORK
-echo ____________________
-echo.
-echo      NETWORKING
-echo ____________________
-echo.
-echo [++ICONFIG]
-ipconfig /allcompartments /all
-echo.
-echo [++MAC Addresses]
-getmac
-echo.
-echo [++Route]
-echo.
-route PRINT
-echo.
-echo [++Netstat]
-netstat -ano
-echo.
-echo [++ARP]
-arp -a
-echo.
-echo [++Firewall Configuration]
-netsh firewall show config
-echo [++Domain]
-echo.
-set userdomain
-echo.
-del systeminfo.txt
-del hotfix.txt
-EXIT /B
-
-:PROCESSES
-echo ___________________
-echo.
-echo      PROCESSES
-echo ___________________
-echo.
-echo [++Tasklist]
-tasklist /v
-echo.
-echo [++Drivers Installed]
-driverquery /vw
-del systeminfo.txt
-del hotfix.txt
-EXIT /B
-
-:USERS
-echo ___________________
-echo.
-echo      USER INFO
-echo ___________________
-echo.
-echo [++Current User]
-echo.
-whoami
-echo.
-echo [++All Users]
-net users
-echo.
-echo [++User Groups]
-net localgroup
-echo.
-del systeminfo.txt
-del hotfix.txt
-EXIT /B
 
 :EXIT
 del systeminfo.txt
